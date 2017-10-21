677c14709a39b1a3da71d05483c0e47a9a9042f8
Oscar <oscarbasilio_5@hotmail.com>
2017-10-20 22:32:38 -0500

    Tema Html

--- Cibertec.WebApiTest/CustomerControllerTests.cs
+++ Cibertec.WebApiTest/CustomerControllerTests.cs
@@ -50,5 +50,60 @@ namespace Cibertec.WebApiTest
             var model = result.Value as List<Customer>;
             model.Count.Should().BeGreaterThan(0);
         }
+
+        [Fact(DisplayName ="[CustomerController] Insert")]
+        public void Insert_customer_Test()
+        {
+            var customer = new Customer
+            {
+                Id = 101,
+                City = "Lima",
+                Country = "peru",
+                FirstName = "Julio",
+                LastName = "Velarde",
+                Phone = "99966333"
+            };
+
+            var result = _customerController.Post(customer) as ObjectResult;
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+
+            var model = Convert.ToInt32(result.Value);
+            model.Should().Be(101);
+        }
+
+        [Fact(DisplayName ="[CustomerController] Update")]
+        public void Update_Customer_Test()
+        {
+            var customer = new Customer
+            {
+                Id = 1,
+                City = "Lima",
+                Country = "peru",
+                FirstName = "Julio",
+                LastName = "Velarde",
+                Phone = "99966333"
+            };
+
+            var result = _customerController.Put(customer) as OkObjectResult;
+
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+
+            var model = result.Value?.GetType().GetProperty("Message").GetValue(result.Value);
+            model.Should().Be("The customer is update");
+
+
+            var currentCustomer = _unitMocked.Customers.GetById(1);
+            currentCustomer.Should().NotBeNull();
+            currentCustomer.Id.Should().Be(customer.Id);
+            currentCustomer.City.Should().Be(customer.City);
+            currentCustomer.Country.Should().Be(customer.Country);
+            currentCustomer.FirstName.Should().Be(customer.FirstName);
+            currentCustomer.LastName.Should().Be(customer.LastName);
+            currentCustomer.Phone.Should().Be(customer.Phone);
+
+
+        }
     }
 }
